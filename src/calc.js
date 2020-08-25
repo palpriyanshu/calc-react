4f7099fbb3c3490f737522f6c49ca674aa3274cb
ayush <58028488+desibabua@users.noreply.github.com>
2020-08-25 13:40:29 +0530

    attached operators in display

--- src/calc.js
+++ src/calc.js
@@ -1,41 +1,40 @@
 import React from 'react';
 
+const numbers = [...Array(10).keys()];
+const functionKeys = ['+', '-', 'x', '÷', '%'];
+
 const Display = (props) => <div className="display">{props.number}</div>;
-
 const Button = (props) => <div className={props.className}>{props.name}</div>;
-
-const keys = [
-  '1',
-  '2',
-  '3',
-  '÷',
-  '4',
-  '5',
-  '6',
-  'x',
-  '7',
-  '8',
-  '9',
-  '+',
-  '0',
-  '=',
-  '%',
-  '-',
-];
 
 class Keyboard extends React.Component {
   constructor(props) {
     super(props);
-    this.concatNumber = this.concatNumber.bind(this);
+    this.handleNumberClick = this.handleNumberClick.bind(this);
+    this.handleSymbolClick = this.handleSymbolClick.bind(this);
   }
 
-  concatNumber(e) {
-    this.props.handleClick(this.props.number + e.target.innerText);
+  handleNumberClick(e) {
+    const {setDisplayState, setNumberState} = this.props.clickHandlers();
+    const value = e.target.innerText;
+    setDisplayState(value);
+    setNumberState(value);
   }
 
-  createKeyboardBtn() {
-    return this.props.keys.map((key, id) => (
-      <div className="button" key={id} onClick={this.concatNumber}>
+  handleSymbolClick(e) {
+    const {
+      setDisplayState,
+      setNumber,
+      setSymbol,
+    } = this.props.clickHandlers();
+    const value = e.target.innerText;
+    setDisplayState(value);
+    setNumber();
+    setSymbol(value);
+  }
+
+  createKeyboardBtn(keys, handleClick) {
+    return keys.map((key) => (
+      <div className="button" key={key} onClick={handleClick}>
         {key}
       </div>
     ));
@@ -46,7 +45,11 @@ class Keyboard extends React.Component {
       <div className="keyboard">
         <Button className="button clrBtn" name="clr" />
         <Button className="button delBtn" name="del" />
-        {this.createKeyboardBtn()}
+        <div className="button" onClick={() => {}}>
+          =
+        </div>
+        {this.createKeyboardBtn(numbers, this.handleNumberClick)}
+        {this.createKeyboardBtn(functionKeys, this.handleSymbolClick)};
       </div>
     );
   }
@@ -55,20 +58,83 @@ class Keyboard extends React.Component {
 class Calc extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { number: 0 };
+    this.state = {
+      currDisplayState: '0',
+      currNumberState: '',
+      numbers: [],
+      symbols: [],
+      answer: null,
+    };
+    this.setDisplayState = this.setDisplayState.bind(this);
+    this.setNumberState = this.setNumberState.bind(this);
     this.setNumber = this.setNumber.bind(this);
+    this.setSymbol = this.setSymbol.bind(this);
+    this.setForAnswer = this.setForAnswer.bind(this);
+    this.clickHandlers = this.clickHandlers.bind(this);
   }
 
-  setNumber(number) {
-    this.setState(() => ({ number: Number(number) }));
+  setDisplayState(keyPressed) {
+    this.setState((state) => {
+      const { currDisplayState } = state;
+      const newState =
+        currDisplayState === '0' ? keyPressed : currDisplayState + keyPressed;
+      return { currDisplayState: newState };
+    });
+  }
+
+  setNumberState(keyPressed) {
+    this.setState((state) => {
+      const { currNumberState } = state;
+      return { currNumberState: currNumberState + keyPressed };
+    });
+  }
+
+  setNumber() {
+    this.setState((state) => {
+      const number = state.currNumberState;
+      const numbers = number
+        ? state.numbers.concat([Number(number)])
+        : state.numbers;
+      return { numbers , currNumberState :''};
+    });
+  }
+
+  setSymbol(symbolPressed) {
+    this.setState((state) => {
+      const { symbols, numbers ,currDisplayState} = state;
+      if (symbols.length >= numbers.length) {
+        return { symbols: symbols.slice(0, -1).concat([symbolPressed]),currDisplayState:currDisplayState.slice(0,-2).concat(symbolPressed)};
+      }
+      return { symbols: symbols.concat([symbolPressed]) };
+    });
+  }
+
+  setForAnswer(answer) {
+    this.setState(() => {
+      return {
+        currDisplayState: answer,
+        currNumberState: answer,
+        number: [answer],
+      };
+    });
+  }
+
+  clickHandlers() {
+    return {
+      setDisplayState: this.setDisplayState,
+      setNumberState: this.setNumberState,
+      setNumber: this.setNumber,
+      setSymbol: this.setSymbol,
+      setForAnswer: this.setForAnswer,
+    };
   }
 
   render() {
-    const { number } = this.state;
+    const { currDisplayState } = this.state;
     return (
       <div className="calc">
-        <Display number={`${number}`} />
-        <Keyboard keys={keys} handleClick={this.setNumber} number={number} />
+        <Display number={`${currDisplayState}`} />
+        <Keyboard clickHandlers={this.clickHandlers} />
       </div>
     );
   }
